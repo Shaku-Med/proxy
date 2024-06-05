3ff32efa17e7056ec4e59e4b7f41b410fbc887c4
Medzy Ems Amara <100793520+Shaku-Med@users.noreply.github.com>
2024-06-05 23:49:17 +0000

    s

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const { URL } = require("url"); // Node.js URL module for parsing URLs
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 // Middleware to parse JSON and urlencoded request bodies
 app.use(express.json());
@@ -12,39 +12,37 @@ app.use(express.urlencoded({ extended: true }));
 // Define a route for proxying requests to a target URL
 app.get("*", async(req, res) => {
     try {
-        let encodedUrl = req.originalUrl.split("/?proxy_med=")[1];
-        if (encodedUrl) {
-            let decodedUrl = decodeURIComponent(encodedUrl);
-            res.send(decodedUrl)
-                // let ul = new URL(decodedUrl);
-                // let response = await axios.get(decodedUrl, {
-                //     headers: {
-                //         referer: `${ul.origin}/`,
-                //     },
-                //     responseType: 'arraybuffer', // Ensure axios treats the response as binary data
-                // });
+        let u = req.url.split("/?proxy_med=")[1];
+        res.send(req.url)
+            // if (u) {
+            //     let ul = new URL(u);
+            //     let response = await axios.get(u, {
+            //         headers: {
+            //             referer: `${ul.origin}/`,
+            //         },
+            //         responseType: "arraybuffer", // Ensure axios treats the response as binary data
+            //     });
 
-            // // Determine Content-Type dynamically based on response headers
-            // let contentType = response.headers['content-type'];
-            // if (contentType.startsWith('image/')) {
-            //     res.setHeader('Content-Type', contentType); // Set Content-Type header for images
-            // } else if (contentType.startsWith('text/html')) {
-            //     res.setHeader('Content-Type', 'text/html'); // Set Content-Type header for HTML
-            // } else if (contentType.startsWith('application/javascript')) {
-            //     res.setHeader('Content-Type', 'application/javascript'); // Set Content-Type header for JavaScript
-            // } else if (contentType.startsWith('text/css')) {
-            //     res.setHeader('Content-Type', 'text/css'); // Set Content-Type header for CSS
-            // } else {
-            //     res.setHeader('Content-Type', contentType); // Fallback to original Content-Type
-            // }
+        //     // Determine Content-Type dynamically based on response headers
+        //     let contentType = response.headers["content-type"];
+        //     if (contentType.startsWith("image/")) {
+        //         res.setHeader("Content-Type", contentType); // Set Content-Type header for images
+        //     } else if (contentType.startsWith("text/html")) {
+        //         res.setHeader("Content-Type", "text/html"); // Set Content-Type header for HTML
+        //     } else if (contentType.startsWith("application/javascript")) {
+        //         res.setHeader("Content-Type", "application/javascript"); // Set Content-Type header for JavaScript
+        //     } else if (contentType.startsWith("text/css")) {
+        //         res.setHeader("Content-Type", "text/css"); // Set Content-Type header for CSS
+        //     } else {
+        //         res.setHeader("Content-Type", contentType); // Fallback to original Content-Type
+        //     }
 
-            // res.send(response.data); // Send the data as response
-        } else {
-            res.status(400).send("Invalid URL");
-        }
+        //     res.send(response.data); // Send the data as response
+        // } else {
+        //     res.status(400).send("Invalid URL");
+        // }
     } catch (e) {
-        console.error("Error fetching URL:", e);
-        res.status(500).send("Internal Server Error");
+        res.status(404).send("Internal Server Error");
     }
 });
 
